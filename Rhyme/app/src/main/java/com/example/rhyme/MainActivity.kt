8406d312f1067dd0d723824994bfb52184233a0c
Daniel Russom <66117306+dannyrussom@users.noreply.github.com>
2021-11-05 19:04:35 +0800

    git test
    this is to test that the commits are going through

--- Rhyme/app/src/main/java/com/example/rhyme/MainActivity.kt
+++ Rhyme/app/src/main/java/com/example/rhyme/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var myText = findViewById<TextView>(R.id.textyou).apply {
-            text = "awesome job"
+            text = "keep it up"
         }
     }
 }
