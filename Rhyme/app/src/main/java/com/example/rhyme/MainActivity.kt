5e5d8bc1bacd482ee54ed3730c9b00ce65d8ce3b
Daniel Russom <66117306+dannyrussom@users.noreply.github.com>
2021-11-05 22:25:30 +0800

    data set, layout design and function
    added data set of 190000 words
    designed layout
    added a function for the button

--- Rhyme/app/src/main/java/com/example/rhyme/MainActivity.kt
+++ Rhyme/app/src/main/java/com/example/rhyme/MainActivity.kt
@@ -2,14 +2,66 @@ package com.example.rhyme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
+import com.example.rhyme.Words
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var myText = findViewById<TextView>(R.id.textyou).apply {
-            text = "keep it up"
+    }
+
+    fun rhyming(v: View){
+        var w = ""
+        var p = 4
+        var v = 'a'
+        var rhymWords = ""
+        var words = Words()
+        var wrds = words.words_raw.split("\n")
+        var vowels = "aeiou"
+        if(findViewById<EditText>(R.id.word).text.toString() != ""){
+            w = findViewById<EditText>(R.id.word).text.toString()
+            if(findViewById<EditText>(R.id.precision).text.toString() != ""){
+                p = findViewById<EditText>(R.id.precision).text.toString().toInt()
+            }
+            if(p > w.length){
+                p = w.length
+            }
+            for (wrd in wrds){
+                if(wrd.length > p){
+                    if(wrd.reversed().substring(0, p).toString() == w.reversed().substring(0, p).toString()){
+                        if(findViewById<CheckBox>(R.id.prefix).isChecked){
+                            for (c in w){
+                                if(c in vowels){
+                                    v = c
+                                    break
+                                }
+                            }
+                            if(v in wrd.substring(0,3)){
+                                rhymWords += "\n"
+                                rhymWords += wrd
+                            }
+                            continue
+                        }
+                        else{
+                            rhymWords += "\n"
+                            rhymWords += wrd
+                        }
+                    }
+                }
+            }
+            var myList = findViewById<TextView>(R.id.list).apply {
+                text = rhymWords + "nothin"
+            }
+        }
+        else{
+            var myList = findViewById<TextView>(R.id.list).apply {
+                text = "write a word first."
+            }
         }
     }
+
 }
